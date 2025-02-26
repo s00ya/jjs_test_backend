fa112e651a480c7ba2d2281731449b52b00d60d9
s00ya <jjschoho@gmail.com>
2025-02-26 09:33:39 +0900

    Initial commit

---
+++ src/test/java/com/example/jjs_test_backend/JjsTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jjs_test_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjsTestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
